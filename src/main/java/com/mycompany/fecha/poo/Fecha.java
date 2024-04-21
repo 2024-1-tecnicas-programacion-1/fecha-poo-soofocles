add7d4febf149b33bbe2b519d0d2a3c47955837a
soofocles <anasfajardol@gmail.com>
2024-04-21 12:56:14 -0500

    Se realizo ejercicio

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,55 @@
 package com.mycompany.fecha.poo;
-
+import java.util.HashMap;
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
+   
+    private int dia;
+    private int mes;
+    private int annio;
+   
+    private final HashMap<Integer, String> meses;
+        
+    public Fecha(int dia, int mes, int annio){
+        this.dia = dia;
+        this.mes = mes;
+        this.annio = annio;
+        this.meses = new HashMap<>();
+        meses.put(1, "enero");
+        meses.put(2, "febrero");
+        meses.put(3, "marzo");
+        meses.put(4, "abril");
+        meses.put(5, "mayo");
+        meses.put(6, "junio");
+        meses.put(7, "julio");
+        meses.put(8, "agosto");
+        meses.put(9, "septiembre");
+        meses.put(10, "octubre");
+        meses.put(11, "noviembre");
+        meses.put(12, "diciembre");
+ }
+
+    public String fechaCorta() {
+        return dia + "/" + mes + "/" + annio;
+    }
+
+    public boolean validarFecha() {
+        boolean validacion = true; // Initialize as true by default
+        boolean annioBisiesto = (annio % 4 == 0 && annio % 100 != 0) || (annio % 400 == 0);
+
+        if (dia <= 0 || dia > 31) {
+            validacion = false;
+        } else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) {
+            validacion = false;
+        } else if (mes == 2) {
+            validacion = (annioBisiesto && dia <= 29) || (!annioBisiesto && dia <= 28);
+        }
+        return validacion;
+    }
+
+    public String mesLetra() {
+        return meses.get(mes);
+    }
+
+    public String fechaLarga() {
+        return dia + " de " + meses.get(mes) + " de " + annio;
+    }
 }
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,16 @@
 package com.mycompany.fecha.poo;
-
+import java.util.Scanner;
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Fecha fecha1; // Definición
+        int dia = sc.nextInt();
+        int mes = sc.nextInt();
+        int annio = sc.nextInt();
+        fecha1 = new Fecha(dia, mes, annio); //Inicialización
+        System.out.println(fecha1.validarFecha());
+        if (fecha1.validarFecha()==true){
+            System.out.println(fecha1.fechaLarga());
+        }
+    }
 }
